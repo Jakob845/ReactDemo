0404fa1ec9e4d47ea07dcf73aeeec3d102a251da
Jakob845 <jakobzetterqvist@hotmail.com>
2022-01-05 15:35:28 +0100

    Structure and First component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react'
+import{MyFirstComponent} from './components/myfirstcomponent/MyFirstComponent'
 
 function App() {
   return (
     <>
       <h1>React Tutorial!</h1>
+      < MyFirstComponent/>
     </>
   );
 }
---
+++ src/components/myfirstcomponent/MyFirstComponent.js
@@ -0,0 +1,6 @@
+import Reace from "react";
+export const MyFirstComponent = () => {
+  return <div>
+      <h1>Det här är min första funtionella React komponent</h1>
+  </div>;
+};
